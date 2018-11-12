e8016358c24ee3ad368b47ede3b3fb3820a04ba1
Brokoli <bocalist@gmail.com>
2018-11-12 12:19:23 +0900

    added some basic link and data from db

--- app.py
+++ app.py
@@ -18,6 +18,22 @@ def showAll():
     recentItems = session.query(CategoryItem).order_by(CategoryItem.datetime.desc()).limit(10)
     return render_template('index.html', title=title, categories=categories, recentItems=recentItems)
 
+@app.route('/category/<int:category_id>')
+def showCategoryItems(category_id):
+    categories = session.query(Category).all()
+    cur_category = session.query(Category).filter_by(id=category_id).one()
+    title = "Items of {}".format(cur_category.name.capitalize())
+    items = session.query(CategoryItem).filter_by(category_id=category_id).order_by(CategoryItem.datetime.desc()).all()
+    return render_template('category.html', title=title, categories=categories, cur_category=cur_category, items=items)
+
+@app.route('/category/<int:category_id>/item/<int:item_id>')
+def showItemDetails(category_id, item_id):
+    categories = session.query(Category).all()
+    cur_category = session.query(Category).filter_by(id=category_id).one()
+    cur_item = session.query(CategoryItem).filter_by(id=item_id).one()
+    title = "Details of {}".format(cur_item.name.capitalize())
+    return render_template('item.html', title=title, cur_category=cur_category, cur_item=cur_item)
+
 
 if __name__ == '__main__':
     app.secret_key = 'super_secret_key'

1a6410f43d94092a893863750d080bf38323de29
Brokoli <bocalist@gmail.com>
2018-11-12 17:15:26 +0900

    added login/logout function with google oauth

--- app.py
+++ app.py
@@ -1,38 +1,159 @@
-from flask import Flask, render_template, request, url_for, redirect, flash, jsonify
+from flask import Flask, render_template, request, url_for, redirect, flash, jsonify, make_response
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Category, CategoryItem, Base
+from flask import session as login_session
+import random, string
+from oauth2client.client import flow_from_clientsecrets
+from oauth2client.client import FlowExchangeError
+import httplib2, json, requests
+
 app = Flask(__name__)
 
-engine = create_engine('sqlite:///category.db')
+engine = create_engine('sqlite:///category.db?check_same_thread=False')
 Base.metadata.bind = engine
 
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
-# Restaurant CRUD
+CLIENT_ID = json.loads(open('client_secret.json', 'r').read())['web']['client_id']
+
+
+# Index page
 @app.route('/')
 def showAll():
+    state = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in xrange(32))
+    login_session['state'] = state
     title = "Welcome to Item Catalog Website"
     categories = session.query(Category).all()
     recentItems = session.query(CategoryItem).order_by(CategoryItem.datetime.desc()).limit(10)
-    return render_template('index.html', title=title, categories=categories, recentItems=recentItems)
+    return render_template('index.html', title=title, categories=categories, recentItems=recentItems, STATE=state, login_session=login_session)
 
 @app.route('/category/<int:category_id>')
 def showCategoryItems(category_id):
+    state = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in xrange(32))
+    login_session['state'] = state
     categories = session.query(Category).all()
     cur_category = session.query(Category).filter_by(id=category_id).one()
     title = "Items of {}".format(cur_category.name.capitalize())
     items = session.query(CategoryItem).filter_by(category_id=category_id).order_by(CategoryItem.datetime.desc()).all()
-    return render_template('category.html', title=title, categories=categories, cur_category=cur_category, items=items)
+    return render_template('category.html', title=title, categories=categories, cur_category=cur_category, items=items, STATE=state, login_session=login_session)
 
 @app.route('/category/<int:category_id>/item/<int:item_id>')
 def showItemDetails(category_id, item_id):
+    state = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in xrange(32))
+    login_session['state'] = state
     categories = session.query(Category).all()
     cur_category = session.query(Category).filter_by(id=category_id).one()
     cur_item = session.query(CategoryItem).filter_by(id=item_id).one()
     title = "Details of {}".format(cur_item.name.capitalize())
-    return render_template('item.html', title=title, cur_category=cur_category, cur_item=cur_item)
+    return render_template('item.html', title=title, cur_category=cur_category, cur_item=cur_item, STATE=state, login_session=login_session)
+
+# Google login connect
+@app.route('/gconnect', methods=['POST'])
+def gconnect():
+    # Validate state token
+    if request.args.get('state') != login_session['state']:
+        response = make_response(json.dumps('Invalid state parameter.'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    # Obtain authorization code
+    code = request.data
+
+    try:
+        # Upgrade the authorization code into a credentials object
+        oauth_flow = flow_from_clientsecrets('client_secret.json', scope='')
+        oauth_flow.redirect_uri = 'postmessage'
+        credentials = oauth_flow.step2_exchange(code)
+    except FlowExchangeError:
+        response = make_response(
+            json.dumps('Failed to upgrade the authorization code.'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # Check that the access token is valid.
+    access_token = credentials.access_token
+    url = ('https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=%s'
+           % access_token)
+    h = httplib2.Http()
+    result = json.loads(h.request(url, 'GET')[1])
+    # If there was an error in the access token info, abort.
+    if result.get('error') is not None:
+        response = make_response(json.dumps(result.get('error')), 500)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # Verify that the access token is used for the intended user.
+    gplus_id = credentials.id_token['sub']
+    if result['user_id'] != gplus_id:
+        response = make_response(
+            json.dumps("Token's user ID doesn't match given user ID."), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # Verify that the access token is valid for this app.
+    if result['issued_to'] != CLIENT_ID:
+        response = make_response(
+            json.dumps("Token's client ID does not match app's."), 401)
+        print "Token's client ID does not match app's."
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    stored_access_token = login_session.get('access_token')
+    stored_gplus_id = login_session.get('gplus_id')
+    if stored_access_token is not None and gplus_id == stored_gplus_id:
+        response = make_response(json.dumps('Current user is already connected.'),
+                                 200)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # Store the access token in the session for later use.
+    login_session['access_token'] = credentials.access_token
+    login_session['gplus_id'] = gplus_id
+
+    # Get user info
+    userinfo_url = "https://www.googleapis.com/oauth2/v1/userinfo"
+    params = {'access_token': credentials.access_token, 'alt': 'json'}
+    answer = requests.get(userinfo_url, params=params)
+
+    data = answer.json()
+
+    login_session['username'] = data['name']
+    login_session['picture'] = data['picture']
+    login_session['gplus_id'] = data['id']
+
+    return 'success!'
+
+# Google login disconnect
+@app.route('/gdisconnect')
+def gdisconnect():
+    access_token = login_session.get('access_token')
+    if access_token is None:
+        print 'Access Token is None'
+        response = make_response(json.dumps('Current user not connected.'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    print 'In gdisconnect access token is %s', access_token
+    print 'User name is: '
+    print login_session['username']
+    url = 'https://accounts.google.com/o/oauth2/revoke?token=%s' % login_session['access_token']
+    h = httplib2.Http()
+    result = h.request(url, 'GET')[0]
+    print 'result is '
+    print result
+    if result['status'] == '200':
+        del login_session['access_token']
+        del login_session['gplus_id']
+        del login_session['username']
+        del login_session['picture']
+        # response = make_response(json.dumps('Successfully disconnected.'), 200)
+        # response.headers['Content-Type'] = 'application/json'
+        # return response
+        return redirect('/')
+    else:
+        response = make_response(json.dumps('Failed to revoke token for given user.', 400))
+        response.headers['Content-Type'] = 'application/json'
+        return response
 
 
 if __name__ == '__main__':
